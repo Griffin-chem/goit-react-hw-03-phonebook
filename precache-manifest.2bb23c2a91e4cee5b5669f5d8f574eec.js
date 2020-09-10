20df2c567c48265b92cd0a5d040a7e13ad6b0fd0
Griffin <chemist_khnu@rambler.ru>
2020-09-10 23:34:30 +0300

    Updates

---
+++ precache-manifest.2bb23c2a91e4cee5b5669f5d8f574eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17dd9fc19c89b4e2fc8112305e568c0a",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "fedc0fe8ca6f403649f8",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b4a79879d9e36ceb886",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.9da0044b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.9da0044b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fedc0fe8ca6f403649f8",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.ff908aaf.chunk.js"
+  },
+  {
+    "revision": "9b7d193cf098db4939f5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.76edf9a8.js"
+  }
+]);
